0e3fa25e30cc16d297b029625a846dd5293f95d5
Hand <kjhand2@keeling-f01.(none)>
2017-02-11 18:03:16 -0600

    fixed homework

--- Week4Answers/Problem1.py
+++ Week4Answers/Problem1.py
@@ -1,6 +1,6 @@
 fleet = 100
 drivers = 30
-passengers = 90
+passengers = 85
 
 unused_cars = fleet - drivers
 
@@ -8,14 +8,14 @@ used_cars = fleet - unused_cars
 
 total_capacity = used_cars * 4
 
-avg_pass = total_capacity / used_cars
+avg_pass = passengers / used_cars
 
 print(unused_cars, 'cars are not used today.')
 
 print(used_cars, 'cars are used today.')
 
-print('The total capacity of the fleet is ', total_capacity)
+print('The total capacity of the fleet is ', total_capacity, '.')
 
-print('The average number of passengers per car is', avg_pass)
+print('The average number of passengers per car is', avg_pass, '.')
 
 print('done with report')
